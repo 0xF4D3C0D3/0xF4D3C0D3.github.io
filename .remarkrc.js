da3e87ebdcd7e1262e618c8995f183a28de6f1b8
Jung Dong Ho <dongho971220@gmail.com>
2019-06-10 00:21:17 +0900

    remark sucks

--- .remarkrc.js
+++
@@ -1,9 +0,0 @@
-exports.plugins = [
-  require('remark-preset-lint-recommended'),
-  require('remark-frontmatter')
-]
-
-exports.settings = {
-  lint-list-item-indent: 'space',
-  ordered-list-marker-value: 'one'
-}
